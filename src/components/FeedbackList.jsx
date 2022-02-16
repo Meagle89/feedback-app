ae1e205a66e748a0d9ff6eb80707cf56a01d1fd6
Chris Meade <chris.meade1989@gmail.com>
2022-02-16 10:47:03 +0000

    added a mock backend and hooked up the frontend

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,20 +2,22 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from "./FeedbackItem"
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 
 function FeedbackList() {
-  const {feedback} = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  if(!feedback || feedback.length === 0 ) {
+  if(!isLoading && (!feedback || feedback.length === 0) ) {
     return(
       <p>No feedback yet</p>
     )
   }
-
-  return (
-    <div className="feedback-list">
-      <AnimatePresence>
+  return isLoading ? (
+    <Spinner />
+  ) : ( 
+  <div className="feedback-list">
+    <AnimatePresence>
       {feedback.map((item) => (
         <motion.div 
         key={item.id}
@@ -28,9 +30,9 @@ function FeedbackList() {
           key={item.id} />
       </motion.div>
       ))}
-      </AnimatePresence>
-    </div>
-  )
+    </AnimatePresence>
+  </div>
+)
 }
 
 
